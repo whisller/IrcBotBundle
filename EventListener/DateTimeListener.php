ce5aab5f67a3da842555ec5d3d670865dc02d56a
whisller <whisller@gmail.com>
2012-09-03 00:56:19 +0200

    Small fixes in DateTimeListener

--- EventListener/DateTimeListener.php
+++ EventListener/DateTimeListener.php
@@ -14,9 +14,14 @@ class DateTimeListener extends BaseListener
 
         $timezone = date_default_timezone_get();
 
-        if (isset($arguments[0])) {
+        if (isset($arguments[0]) && ('' !== trim($arguments[0]))) {
             if (in_array($arguments[0], \DateTimeZone::listIdentifiers())) {
                 $timezone = $arguments[0];
+            } else {
+                $event->getConnection()->sendData((string)new PrivMsgCommand(array('receiver' => $event->getChannel(),
+                                                                                   'text' => (string)new Message(sprintf('Sorry, I don\'t know about %s timezone.', $arguments[0])))));
+
+                return false;
             }
         }
 
