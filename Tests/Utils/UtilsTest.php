ab15568a9c43491e57be1ec76237e4c60c35e03e
Jakub Zalas <jakub@zalas.pl>
2012-09-11 20:27:45 +0100

    Bootstrap phpunit test suite and implemented first test.

---
+++ Tests/Utils/UtilsTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Whisnet\IrcBotBundle\Tests\Utils;
+
+use Whisnet\IrcBotBundle\Utils\Utils;
+
+class UtilsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider provideEndOfLineTexts
+     */
+    public function testThatEndOfLineMarkersAreRemoved($text, $expectedResult)
+    {
+        $result = Utils::cleanUpServeRequest($text);
+
+        $this->assertSame($expectedResult, $result);
+    }
+
+    public function provideEndOfLineTexts()
+    {
+        return array(
+            array('Lorem ipsum', 'Lorem ipsum'),
+            array("Lorem ipsum\r\n", 'Lorem ipsum'),
+            array("\rLorem\nipsum", 'Loremipsum'),
+            array("Lorem \ripsum", 'Lorem ipsum')
+        );
+    }
+}
