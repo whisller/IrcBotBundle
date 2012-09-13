dd3c0676b6739540b3d94b4360e2a41a2b92fa77
whisller <whisller@gmail.com>
2012-09-13 21:07:07 +0200

    Fixed class name.

--- IrcCommands/TopicCommand.php
+++ IrcCommands/TopicCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Validator\Constraints\NotBlank;
  *
  * @author Daniel Ancuta <whisller@gmail.com>
  */
-class JoinCommand extends Command
+class TopicCommand extends Command
 {
     /**
      * @var string
