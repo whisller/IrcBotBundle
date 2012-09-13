2ff3d7798d22475cd0b0c01e23ac106898b942b0
whisller <whisller@gmail.com>
2012-09-13 22:24:14 +0200

    Added phpunit tests.

---
+++ Tests/IrcCommands/ListCommandTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace Whisnet\IrcBotBundle\Tests\IrcCommands;
+
+use Whisnet\IrcBotBundle\IrcCommands\ListCommand;
+
+class ListCommandTest extends \PHPUnit_Framework_TestCase
+{
+    public function test__toStringEmptyList()
+    {
+        $this->assertEquals("LIST\r", (string)new ListCommand());
+    }
+
+    public function test__toStringOneChannel()
+    {
+        $this->assertEquals("LIST #foo\r", (string)new ListCommand(array('#foo')));
+    }
+
+    public function test__toStringTwoChannels()
+    {
+        $this->assertEquals("LIST #foo,#bar\r", (string)new ListCommand(array('#foo', '#bar')));
+    }
+}
